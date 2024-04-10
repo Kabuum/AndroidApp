993138afed5c83dd8d3dc4fb8446220820dfdf4c
Kabuum <thomsenribermartin@gmail.com>
2024-04-10 12:48:49 +0200

    location function

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -10,22 +10,69 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.example.weatherapp.ui.theme.WeatherAppTheme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+
 
 class MainActivity : ComponentActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        GetCoords()
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
+
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    Greeting("Martin")
                 }
             }
         }
+    }
+
+    private fun GetCoords(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED)  {
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location ->
+                    if (location != null) {
+                        val latitude = location.latitude
+                        val longitude = location.longitude
+                        val crs84Coords = "POINT($longitude $latitude)"
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(this@MainActivity, "Failed to retrieve location. Please enable location permissions.", Toast.LENGTH_SHORT).show()
+                }
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                PERMISSION_REQUEST_LOCATION
+            )
+
+        }
+    }
+
+    companion object {
+        private const val PERMISSION_REQUEST_LOCATION = 100
     }
 }
 
@@ -41,6 +88,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     WeatherAppTheme {
-        Greeting("Android")
+        Greeting("Android",)
     }
 }
