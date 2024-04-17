bbc9a6e4a9c4b41c7dd0ed2db336d6546730daed
mreriksen <eriksen2608@gmail.com>
2024-04-17 14:07:11 +0200

    UI
    UI

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.weatherapp
 
-import android.graphics.drawable.Drawable
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement.Absolute.Center
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 
 import androidx.compose.material3.Text
@@ -14,23 +14,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material3.Surface
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.MutableStateFlow
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.compose.viewModel
+
 
 
 class MainActivity : ComponentActivity() {
@@ -44,7 +50,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppTheme {
-                CurrentWeatherDisplay(viewModel = ViewModel, modifier = Modifier)
+                //CurrentWeatherDisplay(viewModel = ViewModel, modifier = Modifier)
+                UI(viewModel = ViewModel, backgroundColor = Color.White, modifier = Modifier)
             }
         }
     }
@@ -90,13 +97,58 @@ fun CurrentWeatherDisplay(viewModel: WeatherViewModel, modifier: Modifier){
         }
     }
 }
+
+@Composable
+fun UI (viewModel: WeatherViewModel, backgroundColor: Color, modifier: Modifier = Modifier) {
+    var state = viewModel.weatherData.collectAsState()
+    Card(
+        backgroundColor = getBackgroundColor(state.value?.weather_code),
+        shape = RoundedCornerShape(10.dp),
+        modifier = modifier.padding(16.dp),
+
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Image(painter = painterResource(id = convertWeatherCodeToImage(state.value?.weather_code)), contentDescription = convertWeatherCodeToString(state.value?.weather_code))
+            Spacer(modifier = Modifier.height(16.dp))
+            androidx.compose.material.Text(
+                text = "${state.value?.temperature_2m}°C",
+                fontSize = 50.sp,
+                color = Color.Black
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = convertWeatherCodeToString(state.value?.weather_code),
+                fontSize = 20.sp,
+                color = Color.Black
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+
+                Image( painterResource(id = R.drawable.humidity_percentage_wght400_opsz48), contentDescription = null)
+                Text(text = "${state.value?.relative_humidity_2m} %")
+                Image(painterResource(id = R.drawable.air_wght400_opsz48), contentDescription =null)
+                Text(text = "${state.value?.wind_speed_10m} km/h")
+            }
+        }
+    }
+}
 fun convertWeatherCodeToImage(weatherCode: Int?): Int{
     val weatherCodeConverted = when(weatherCode){
-        0 -> R.drawable.sunny
-        1,2,3 -> R.drawable.cloudy
-        51, 53, 55, 56, 57, 61, 63 ,65, 66, 67 -> R.drawable.rainy
-        45, 48 -> R.drawable.foggy
-        else -> R.drawable.cloudy
+        0 -> R.drawable.sunny_48px
+        1,2,3 -> R.drawable.cloud_48px
+        51, 53, 55, 56, 57, 61, 63 ,65, 66, 67 -> R.drawable.rainy_48px
+        45, 48 -> R.drawable.foggy_wght400_opsz48
+        else -> R.drawable.cloud_48px
     }
     return weatherCodeConverted
 }
